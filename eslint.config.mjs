5775d1a607b58481ceab2f6faabb7da1732e2524
Huafu Gandon <huafu.gandon@gmail.com>
2024-10-27 15:31:45 +0100

    fix: Fixes tsconfig and eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,14 +4,15 @@ import eslint from "@eslint/js";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+    {
+        ignores: ["dist", "node_modules", "eslint.config.mjs"],
+    },
     eslint.configs.recommended,
     ...tseslint.configs.recommendedTypeChecked,
     {
         languageOptions: {
             parserOptions: {
-                projectService: {
-                    allowDefaultProject: ["*.mjs", "*.js"],
-                },
+                projectService: true,
                 tsconfigRootDir: import.meta.dirname,
             },
         },
