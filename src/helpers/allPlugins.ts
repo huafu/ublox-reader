842036bd4a2a36b71d505a94440f9af85dfffb6f
Huafu Gandon <huafu.gandon@gmail.com>
2024-11-12 12:40:20 +0100

    fix: Updates list of plugins

--- src/helpers/allPlugins.ts
+++ src/helpers/allPlugins.ts
@@ -1,8 +1,13 @@
 import UbloxReaderConsolePlugin from "../plugins/UbloxReaderConsolePlugin";
 import UbloxReaderMqttPlugin from "../plugins/UbloxReaderMqttPlugin";
 import UbloxReaderPlugin from "../plugins/UbloxReaderPlugin";
+import UbloxReaderTpvPlugin from "../plugins/UbloxReaderTpvPlugin";
 import { PluginConfig } from "./config";
 
 export default function allPlugins(): UbloxReaderPlugin<PluginConfig>[] {
-    return [new UbloxReaderConsolePlugin(), new UbloxReaderMqttPlugin()];
+    return [
+        new UbloxReaderConsolePlugin(),
+        new UbloxReaderMqttPlugin(),
+        new UbloxReaderTpvPlugin(),
+    ];
 }
